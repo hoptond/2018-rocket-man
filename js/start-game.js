0d1fa7891a7e8efdce698023a73ec1abbd300a87
Felix <felixgate13@gmail.com>
2018-10-15 16:57:57 +0100

    created rocket animation, added title with animation, added normalize added images, added play button with js

---
+++ js/start-game.js
@@ -0,0 +1,21 @@
+
+
+
+$(function () {
+    var $rocketImg = $('#splash-title-rocket')
+    var $title = $('#splash-title')
+    $rocketImg.animate({
+        left: "+=5000",
+    }, 3000)
+
+    setTimeout(function(){
+     $title.fadeIn(50)
+    }
+    , 800)
+
+    document.getElementById('splash-open-game').addEventListener("click", function(){
+        document.getElementById('splash-screen').style.display = 'none'
+    })
+
+})
+
