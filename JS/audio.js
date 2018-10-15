aee3705c6847600340a453710705346e65691849
Patrick <patrick.kenyon@gmail.com>
2018-10-15 16:57:30 +0100

    added cheers to keys and clicks, need to link to html file

---
+++ JS/audio.js
@@ -0,0 +1,42 @@
+var audioObj = {
+    cheer1: '../audio/cheer1.wav',
+    cheer2: '../audio/cheer2.wav',
+    cheer3: '../audio/cheer3.wav',
+    cheer4: '../audio/cheer4.wav'
+}
+
+document.addEventListener('keypress', function(e) {
+    switch (true) {
+        case (e.key === 'q'):
+            makeNoise('cheer1', audioObj)
+            break
+        case (e.key === 'w'):
+            makeNoise('cheer2', audioObj)
+            break
+        case (e.key === 'e'):
+            makeNoise('cheer3', audioObj)
+            break
+        case (e.key === 'r'):
+            makeNoise('cheer4', audioObj)
+            break
+    }
+})
+
+document.querySelector('#city-1').addEventListener('click', function() {
+    makeNoise('cheer1', audioObj)
+})
+document.querySelector('#city-2').addEventListener('click', function() {
+    makeNoise('cheer2', audioObj)
+})
+document.querySelector('#city-3').addEventListener('click', function() {
+    makeNoise('cheer3', audioObj)
+})
+document.querySelector('#city-4').addEventListener('click', function() {
+    makeNoise('cheer4', audioObj)
+})
+
+
+function makeNoise(noise, audioObject) {
+    var audio = new Audio(audioObject[noise])
+    audio.play()
+}
