23d5fac8fb26ee953f34398b4e08f7791462adeb
Felix <felixgate13@gmail.com>
2018-10-16 11:09:50 +0100

    made deleted whitespace changed gif file name, changed colors to hex codes.

--- js/start-game.js
+++ js/start-game.js
@@ -1,20 +1,13 @@
-
-
-
 $( window ).on( "load", function () {
     var $rocketImg = $('#splash-title-rocket')
     var $title = $('#splash-title')
     $rocketImg.animate({
         left: "+=5000",
     }, 3000)
-
     setTimeout(function(){
      $title.fadeIn(50)
     }
     , 800)
-
-
-
 })
 
 document.getElementById('splash-open-game').addEventListener("click", function(){
