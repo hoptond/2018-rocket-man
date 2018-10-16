af8515b3c0bfa0c145d9565a541175510c28c94b
Daniel <hoptond848@protonmail.com>
2018-10-16 14:05:40 +0100

    removed whitespace

--- js/anims-lasers.js
+++ js/anims-lasers.js
@@ -4,22 +4,6 @@ $(function() {
     })
 })
 
-// document.addEventListener('keypress', function(e) {
-//     switch (true) {
-//         case (e.key === 'q'):
-//             shootLaser($('#city-1'))
-//             break
-//         case (e.key === 'w'):
-//             shootLaser($('#city-2'))
-//             break
-//         case (e.key === 'e'):
-//             shootLaser($('#city-3'))
-//             break
-//         case (e.key === 'r'):
-//             shootLaser($('#city-4'))
-//             break
-//     }
-// })
 
 function shootLaser($city) {
     var cityid = $city.data('city')
