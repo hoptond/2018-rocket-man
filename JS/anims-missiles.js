5862d3355551d1d9908f7cffefd04ad1407b3037
Felix <felixgate13@gmail.com>
2018-10-16 16:32:36 +0100

    ^^

--- JS/anims-missiles.js
+++ JS/anims-missiles.js
@@ -1,11 +1,25 @@
-function dropMissile(columnNumber, animationTime) {
+setTimeout(repeatAnims, 500)
+repeatAnims()
+
+function repeatAnims() {
+    setInterval( function () {
+        dropMissile(1000)
+    }, 1000 )
+}
+
+function dropMissile(animationTime) {
+    var columnNumber = randomNumGen()
     $('#pos-'+columnNumber).prepend('<img src="img/missile-drop-down-white.png" class="missile" id="bomb-'+ columnNumber +'">')
 
     $('#bomb-' + columnNumber).animate({
-        top: "+=350"
+        top: "+=550"
     }, animationTime, "linear", function () {
         $(this).remove()
-
     })
 }
-// run on start button
+
+function randomNumGen() {
+    var randomNum = Math.floor(Math.random() * 4) + 1
+    return(randomNum)
+}
+
