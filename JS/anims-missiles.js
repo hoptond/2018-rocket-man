9a201899c445da7d4f3e52b0f333fdfc71eb159e
Imogen <imogenhallett@hotmail.com>
2018-10-17 13:40:53 +0100

    isHit function has been created and attached to createBombEventHandle function

--- JS/anims-missiles.js
+++ JS/anims-missiles.js
@@ -1,14 +1,20 @@
+//global scope
+var gameScore = {
+    score:0,
+    toll:0
+}
+
 setTimeout(repeatAnims, 500)
 repeatAnims()
 
 function repeatAnims() {
     setInterval(function() {
-        dropMissile(1000)
+        dropMissile(10000)
     }, 1000)
 }
 
 function dropMissile(animationTime) {
-    var columnNumber = randomNumGen()
+    var columnNumber = randomNumGen(4)
     var $bomb = $('<img src="img/missile-drop-down-white.png" ' +
         'class="missile bomb-'+ columnNumber +'">')
     $('#pos-'+columnNumber).prepend($bomb)
@@ -16,42 +22,63 @@ function dropMissile(animationTime) {
     $bomb.animate({
         top: "+=550"
     }, animationTime, "linear", function () {
+        missileHitsCity()
         $(this).remove()
     })
 }
 
-function randomNumGen() {
-    var randomNum = Math.ceil(Math.random() * 4)
+function missileHitsCity() {
+    gameScore.score -= 1
+    console.log(gameScore.score)
+    gameScore.toll += randomNumGen(10000)
+    document.querySelector('#score').textContent = gameScore.score
+    document.querySelector('#toll').textContent = gameScore.toll
+}
+
+function randomNumGen(topLimit) {
+    var randomNum = Math.ceil(Math.random() * topLimit)
     return(randomNum)
 }
 
-function explodeMissile() {
+
+function createBombEventHandler() {
     var cities = document.querySelectorAll('.city')
 
     cities.forEach(function(city) {
-        city.addEventListener('click', function () {
-            var cityNumber = this.getAttribute('data-city')
-            var bombNumber = '.bomb-' + cityNumber
-            var hasBombs = document.querySelectorAll(bombNumber).length
-            console.log(hasBombs)
-            if (hasBombs) {
-                $(bombNumber).stop()
-                var bombs = document.querySelectorAll(bombNumber)
-                bombs.forEach(function (bomb) {
-                        bomb.classList.remove(bombNumber)
-                        bomb.src = "img/missile-explosion.gif";
-                        setTimeout(function () {
-                            try {
-                                bomb.parentNode.removeChild(bomb);
-                            } catch (e) {
-                                // squash errors where the bomb is already exploding and we don't need to remove it.
-                            }
-                        }, 500)
-                })
-            }
-
+        city.addEventListener('click', function() {
+            var that = this
+            isHit(that)
         })
     })//end forEach
 }
 
-explodeMissile()
+createBombEventHandler()
+
+
+function isHit(that) {
+    var cityNumber = that.getAttribute('data-city')
+    var bombNumber = '.bomb-' + cityNumber
+    var hasBombs = document.querySelectorAll(bombNumber).length
+    if (hasBombs) {
+        gameScore.score += hasBombs
+        document.querySelector('#score').textContent = gameScore.score
+        console.log(gameScore.score)
+        $(bombNumber).stop()
+        var bombs = document.querySelectorAll(bombNumber)
+        bombs.forEach(function (bomb) {
+            bomb.classList.remove(bombNumber)
+            bomb.src = "img/missile-explosion.gif";
+
+            setTimeout(function () {
+                try {
+                    bomb.parentNode.removeChild(bomb);
+                } catch (e) {
+                    // squash errors where the bomb is already exploding and we don't need to remove it.
+                }
+            }, 500)
+        })
+    } else {
+        gameScore.score -= 1
+        document.querySelector('#score').textContent = gameScore.score
+    }
+}
