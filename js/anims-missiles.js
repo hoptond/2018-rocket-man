038d9af8fb4b5ed39d09b3a20c23b9e1b7a0d828
Daniel <hoptond848@protonmail.com>
2018-10-18 16:50:47 +0100

    extracted functions from audio, put them into anims-missiles

--- js/anims-missiles.js
+++ js/anims-missiles.js
@@ -170,6 +170,36 @@ function hitCityEffect(cityid) {
     }, 1250)
 }
 
+/*
+ * Fires the laser from the given city.
+ *
+ * @param $city the JQUERY object referring to the given city
+ */
+function shootLaser($city) {
+    $city.addClass('laser')
+    setTimeout(function() {
+        $city.removeClass('laser')
+    }, 300)
+}
+
+/*
+ * This produces a sound from the given short reference.
+ *
+ * @param noise A shortened noise to refer to a file in the audio folder.
+ */
+function makeNoise(noise) {
+    var audioObj = {
+        cheer1: 'audio/cheer1.mp3',
+        cheer2: 'audio/cheer2.mp3',
+        cheer3: 'audio/cheer3.mp3',
+        cheer4: 'audio/cheer4.mp3',
+        hitcity: 'audio/hit_city.mp3',
+        miss: 'audio/laser_miss.mp3'
+    }
+    var audio = new Audio(audioObj[noise])
+    audio.play()
+}
+
 document.querySelector('#start').addEventListener('click', function(e) {
     repeatAnims()
     missilesActive = true
